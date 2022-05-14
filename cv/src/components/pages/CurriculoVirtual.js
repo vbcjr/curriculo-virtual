d13cf03dc8f93ec23e57e485ee5c683325524ed3
Lucas Siq <lucasdassiqueira@gmail.com>
2022-05-13 22:19:13 -0300

    second commit

--- cv/src/components/pages/CurriculoVirtual.js
+++ cv/src/components/pages/CurriculoVirtual.js
@@ -1,5 +1,74 @@
+import React from 'react';
+import styles from './layouts/DadosPessoais.module.css'
+import { useForm } from 'react-hook-form';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import fotoperfil from '../assets/fotoperfil.png'
+
 function CurriculoVirtual() {
-    return <h1> Currículo Virtual</h1>
+    return (
+        <section className={styles.main4}>
+        <h2> Resumo do seu <span className={styles.span1}>Currículo Virtual:</span></h2>
+
+        <div className={styles.perfilfinal}>
+            <img src={fotoperfil} alt="logo" />
+            <div className={styles.textperfilfinal}>
+            <h1> Alan dos Santos Vicente</h1>
+            <p>Brasileiro, 28 anos</p>
+            </div>
+        </div>
+        <div className={styles.contatofinal}>
+            <div className={styles.textcontatofinal}>
+                <h3>Contato</h3>
+                <p>alan.santos@gmail.com</p>
+                <p>11 965843654</p>
+            </div>
+            <div className={styles.enderecocontatofinal}>
+                <h3>Endereço</h3>
+                <p>Av. Augusto Oscar, 6586 - Jardim Paulista, São Paulo, SP - CEP 65896-965</p>
+            </div>
+
+        </div>
+        <div className={styles.objetivofinal}>
+            <h3>Objetivo Profissional</h3>
+            <div className={styles.textobjetivofinal}>
+                <p>Auxiliar Administrativo</p>
+            </div>
+        </div>
+        <div className={styles.formacaofinal}>
+            <h3>Formação</h3>
+            <p>Fiap</p>
+            <p>Graduação em Admistração</p>
+            <p>Administração</p>
+            <p>São Paulo, SP</p>
+            <p>2 anos</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <hr></hr>
+        </div>
+
+        
+
+        <div className={styles.experienciafinal}>
+            <h3>Experiência</h3>
+            <p>Auxiliar Administrativo</p>
+            <p>Tempo Integral</p>
+            <p>Loft S. A.   </p>
+            <p>São Paulo, SP</p>
+            <p>2 anos</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <hr></hr>
+        </div>
+        <div className={styles.hobbiesfinal}>
+            <h3>Hobbies</h3>
+            <p>Blog</p>
+            <p>5 anos</p>
+            <p>Semanal</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+
+        <button type='submit' className={styles.buttonfinal}>Compartilhar </button>
+        </section>
+
+    )
 }
 
 export default CurriculoVirtual
--- cv/src/components/pages/Hobbies.js
+++ cv/src/components/pages/Hobbies.js
@@ -1,5 +1,56 @@
+import React from 'react';
+import styles from './layouts/DadosPessoais.module.css'
+import { useForm } from 'react-hook-form';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {BsArrowRight} from 'react-icons/bs';
+import {BsArrowLeft} from 'react-icons/bs';
+
 function Hobbies() {
-    return <h1> Hobbies</h1>
+
+    const {register, handleSubmit, setValue, formState: { errors }} = useForm();
+
+    const onSubmit = (e) => {
+        console.log(e);
+        alert("Seus dados foram Salvos!");
+    }
+
+    return (
+                
+        <section className={styles.main3}>
+        <h2> Finalizes com os dados de <span className={styles.span1}>Hobbies:</span></h2>
+
+        <div className={styles.form}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <h5>Título do Hobbie</h5>
+                <label className={styles.field2} htmlFor="titulohobbie">
+                    <input className={styles.titulohobbie} type='text' {...register('titulohobbie   ')} placeholder='Blog'></input>
+                </label>
+
+                <h5>Tempo de prática</h5>
+                <label className={styles.field3} htmlFor="tempopratica">
+                    <input className={styles.tempopratica} type='text' {...register('tempopratica')} placeholder='5 Anos'></input>
+                </label>
+
+                <h5>Frequência de prática</h5>
+                <label className={styles.field2} htmlFor="frequenciapratica">
+                    <input className={styles.frequenciapratica} type='text' {...register('frequenciapratica')} placeholder='Semanalmente'></input>
+                </label>
+
+                <h5>Descrição</h5>
+                <label className={styles.field3} htmlFor="descricao3">
+                    <input className={styles.descricao3} type='text' {...register('descricao3')} placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod '></input>
+                </label> 
+                <div className={styles.botoes}>
+                <button className={styles.Button4}><span className={styles.arrowbutton1}><BsArrowLeft /> </span><Link to="/Experiencia" >Voltar <span className={styles.arrowbutton}><BsArrowRight /> </span></Link></button>
+                
+                <button type="submit" className={styles.Button3}><Link to="/CurriculoVirtual" >Avançar <span className={styles.arrowbutton}><BsArrowRight /> </span></Link></button>
+                </div>
+            </form>
+        </div> 
+
+        </section>
+        
+    )
 }
 
 export default Hobbies
